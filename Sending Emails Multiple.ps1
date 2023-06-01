ac77d671a3f7ea2f13b424a2a43498e2c5853605
Vienas <67314023+vienas@users.noreply.github.com>
2023-06-01 15:27:57 +0200

    Add files via upload
    Many emails on the one time from file txt; Message and attachments

---
+++ Sending Emails Multiple.ps1
@@ -0,0 +1,58 @@
+﻿function Get-SendEmails {
+    [CmdletBinding(SupportsShouldProcess=$true, ConfirmImpact='Medium')]
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$SendDW,
+
+        [Parameter(Mandatory=$false)]
+        [string]$SendUDW
+    )
+
+    Process {
+        $Data = Get-Content -Path 'C:\Users\itmw\Documents\data1.txt'
+
+        foreach ($D in $Data) {
+            $ComputerName = $($D.Split("|")[0])
+            $EmailAddress = $($D.Split("|")[1])
+
+            $Outlook = New-Object -ComObject Outlook.Application
+            $Email = $Outlook.CreateItem(0)
+            $Email.Recipients.Add($EmailAddress) | Out-Null
+            $Recip = $Email.Recipients.Add($SendDW)
+            $Recip.Type = 2
+
+            if (![string]::IsNullOrEmpty($SendUDW)) {
+                $Recip2 = $Email.Recipients.Add($SendUDW) 
+                $Recip2.Type = 3
+            }
+
+            $Email.Subject = "Bitlocker na komputerze $ComputerName"
+            $Email.Body = @"
+Cześć,
+
+Na Twoim komputerze $ComputerName jest wyłączony Bitlocker.
+
+Prosimy o informację zwrotną po włączeniu funkcji Bitlocker (wszystkie partycje).
+
+Komputer wymaga wpięcia do sieci firmowej.
+
+W razie pytań prosimy o kontakt.
+
+Administratorzy IT
+"@
+
+            $Email.Attachments.Add("C:\bitlocker.png") | Out-Null
+            $Email.Attachments.Add("C:\bitlocker2.png") | Out-Null
+            $Email.Attachments.Add("C:\bitlocker3.png") | Out-Null
+
+            $Email.Send()
+
+            Write-Host -ForegroundColor Cyan "Email was sent to $EmailAddress"
+            Start-Sleep -Seconds 1
+        }
+    }
+
+    End {
+        Write-Host -ForegroundColor Red "End of file"
+    }
+}
