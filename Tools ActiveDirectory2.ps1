a7860b110cc09b53f96e04bf1bd845a40f6a39a0
Vienas <67314023+vienas@users.noreply.github.com>
2025-03-18 17:48:38 +0100

    Update Tools ActiveDirectory2.ps1

--- Tools ActiveDirectory2.ps1
+++ Tools ActiveDirectory2.ps1
@@ -1,22 +1,22 @@
 ﻿Clear-Host
 Write-Host "1. OBIEKTY NIEZALOGOWANE DO DOMENY X DNI"
-Write-Host "2. ZNAJDŹ OBIEKTY BEZ MENADŻERA ou=wasko,dc=wasko,dc=pl"
+Write-Host "2. ZNAJDŹ OBIEKTY BEZ MENADŻERA"
 Write-Host "3. ZNAJDŹ OBIEKTY UŻYTKOWNIKA"
-Write-Host "4. ZNAJDŹ OBIEKTY WYŁĄCZONE OU=WASKO I PRZENIEŚ DO OU=wylaczone komputery"
-Write-Host "5. L4 - WYSZUKAJ OSOBY NA ZWOLNIENIU LEKARSKIM"
+Write-Host "4. ZNAJDŹ OBIEKTY WYŁĄCZONE "
+Write-Host "5. L4 - WYSZUKAJ"
 Write-Host "6. SZUKAJ WYŁĄCZONYCH UŻYTKOWNIKÓW OU=WASKO I PRZENIEŚ DO OU=wylaczone konta"
 Write-Host "7. SZUKAJ UŻYTKOWNIKA PO TELEFONIE"
-Write-Host "8. SZUKAJ UŻYTKOWNIKA BEZ MANAGERA W DOMENIE ENTE.LOCAL"
-Write-Host "9. SZUKAJ UŻYTKOWNIKA BEZ MANAGERA W DOMENIE WASKO.PL"
+Write-Host "8. SZUKAJ UŻYTKOWNIKA BEZ MANAGERA W DOMENIE ENTE"
+Write-Host "9. SZUKAJ UŻYTKOWNIKA BEZ MANAGERA W DOMENIE WASKO"
 
 $case = $(Write-Host "Wybierz numer zadania: " -ForegroundColor Cyan -NoNewLine; Read-Host);
 
-$Credentials = Get-StoredCredential -Target wasko.pl
-$Credentials_Ente = Get-StoredCredential -Target ente.local
+$Credentials = Get-StoredCredential -Target wasko
+$Credentials_Ente = Get-StoredCredential -Target ente
 $Username = $Credentials.UserName
 $Password = $Credentials.Password
-$Server = "wasko.pl"
-$Server_Ente = "ente.local"
+$Server = "wasko"
+$Server_Ente = "ente"
 $GLOBAL:Credentials = New-Object System.Management.Automation.PSCredential $Username,$Password
 
 switch ($case) {
@@ -27,9 +27,9 @@ switch ($case) {
     Write-Host ""
     $days = Read-Host "Podaj liczbę dni (empty=all)"
     $Date = (Get-Date).AddDays(-$days)
-    Get-ADComputer -Filter {((Enabled -eq $true) -and (LastLogonDate -lt $date))} -Properties LastLogonDate -Credential $Credentials -Server $Server -SearchBase "ou=wasko,dc=wasko,dc=pl" | Select-Object Name, LastLogonDate | Sort-Object LastLogonDate
+    Get-ADComputer -Filter {((Enabled -eq $true) -and (LastLogonDate -lt $date))} -Properties LastLogonDate -Credential $Credentials -Server $Server -SearchBase "l" | Select-Object Name, LastLogonDate | Sort-Object LastLogonDate
 
-    $countobject = (Get-ADComputer -Filter {((Enabled -eq $true) -and (LastLogonDate -lt $date))} -Properties LastLogonDate -Credential $Credentials -Server $Server -SearchBase "ou=wasko,dc=wasko,dc=pl" | Select-Object LastLogonDate).count
+    $countobject = (Get-ADComputer -Filter {((Enabled -eq $true) -and (LastLogonDate -lt $date))} -Properties LastLogonDate -Credential $Credentials -Server $Server -SearchBase "1" | Select-Object LastLogonDate).count
 
     Get-ADComputer -Filter {((Enabled -eq $true) -and (LastLogonDate -lt $date))} -Properties DNSHostName, LastLogonDate, whenCreated, OperatingSystem, OperatingSystemVersion, DistinguishedName -Credential $Credentials -Server $Server -SearchBase "ou=wasko,dc=wasko,dc=pl" | Select-Object DNSHostName, LastLogonDate, whenCreated, OperatingSystem, OperatingSystemVersion, DistinguishedName | Sort-Object LastLogonDate | Out-File C:\Lista_obiektów-$countobject.txt
 
@@ -40,11 +40,11 @@ switch ($case) {
     Invoke-Item C:\Lista_obiektów-$countobject.txt
 
     $next1 = Read-Host "Czy przenieść obiekty do OU=wylaczone komputery ? Tak [Tak]"
-    if ($next1 -eq "tak") { Get-ADComputer -Filter {((Enabled -eq $true) -and (LastLogonDate -lt $date))} -Credential $Credentials -Server $Server -SearchBase "ou=wasko,dc=wasko,dc=pl" | Move-ADObject -TargetPath "ou=wylaczone komputery, dc=wasko, dc=pl" }
+    if ($next1 -eq "tak") { Get-ADComputer -Filter {((Enabled -eq $true) -and (LastLogonDate -lt $date))} -Credential $Credentials -Server $Server -SearchBase "1" | Move-ADObject -TargetPath "1" }
     else { break }
 
     $next2 = Read-Host "Czy wyłączyć przeniesione obiekty ? Tak [Tak]"
-    if ($next2 -eq "tak") { Get-ADComputer -Filter {((Enabled -eq $true) -and (LastLogonDate -lt $date))} -Credential $Credentials -Server $Server -SearchBase "ou=wylaczone komputery, dc=wasko, dc=pl" | Disable-ADAccount }
+    if ($next2 -eq "tak") { Get-ADComputer -Filter {((Enabled -eq $true) -and (LastLogonDate -lt $date))} -Credential $Credentials -Server $Server -SearchBase "1" | Disable-ADAccount }
     else { break }
     Write-Host -ForegroundColor DarkYellow "End file"
     $back = Read-Host "Powrót do ekranu głównego ? T [Tak]"
@@ -56,10 +56,10 @@ switch ($case) {
     cls
     do {
     cls
-    $resultCount = (Get-ADComputer -LDAPFilter "(!managedby=*)" -Credential $Credentials -Server $Server -Properties name -SearchBase "ou=wasko,dc=wasko,dc=pl" | Measure-Object).count
+    $resultCount = (Get-ADComputer -LDAPFilter "(!managedby=*)" -Credential $Credentials -Server $Server -Properties name -SearchBase "1" | Measure-Object).count
     
 
-    Get-ADComputer -ldapFilter "(!managedby=*)" -Credential $Credentials -Server $Server -Properties ManagedBy -SearchBase "ou=wasko,dc=wasko,dc=pl" | FT Name, DistinguishedName -Wrap
+    Get-ADComputer -ldapFilter "(!managedby=*)" -Credential $Credentials -Server $Server -Properties ManagedBy -SearchBase "1" | FT Name, DistinguishedName -Wrap
     Write-Host "Liczba znalezionych obiektów: $resultCount"
     Write-Host -ForegroundColor DarkYellow "End file"
 
@@ -85,7 +85,7 @@ switch ($case) {
     '4' {
     cls
     do {
-            $disabledObjects = Get-ADComputer -Filter { Enabled -eq $false } -Credential $Credentials -Server $Server -SearchBase "ou=wasko,dc=wasko,dc=pl" | Select-Object Name
+            $disabledObjects = Get-ADComputer -Filter { Enabled -eq $false } -Credential $Credentials -Server $Server -SearchBase "1" | Select-Object Name
 
             $disabledObjects | Format-List Name
 
@@ -94,7 +94,7 @@ switch ($case) {
     $next4 = Read-Host "Czy przenieść obiekty do OU=wylaczone komputery? (Tak/Nie)"
             
             if ($next4 -eq "Tak") {
-                Get-ADComputer -Filter { Enabled -eq $false } -Credential $Credentials -Server $Server -SearchBase "dc=wasko,dc=pl" | Move-ADObject -TargetPath "ou=wylaczone komputery,dc=wasko,dc=pl"
+                Get-ADComputer -Filter { Enabled -eq $false } -Credential $Credentials -Server $Server -SearchBase "1" | Move-ADObject -TargetPath "1"
             }
 
             Write-Host -ForegroundColor DarkYellow "End file"
@@ -106,19 +106,19 @@ switch ($case) {
     '5' {  
     cls
     do {
-    $Credentials = Get-StoredCredential -Target ente.local
+    $Credentials = Get-StoredCredential -Target ente
     $Username = $Credentials.UserName
     $Password = $Credentials.Password
     $Server = "ente.local"
 
-    $Credentials = Get-StoredCredential -Target fk.wasko.pl
+    $Credentials = Get-StoredCredential -Target fk
     $Username = $Credentials.UserName
     $Password = $Credentials.Password
-    $Server = "fk.wasko.pl"
+    $Server = "fk"
 
- (Get-ADUser -filter * -Properties SamAccountName, description, Company -Server 'wasko.pl' | Where-Object description -like "*nieobecność pow. 14 dni*" | select Company, SamAccountName, description)
-,(Get-ADUser -filter * -Properties SamAccountName, description, Company -Server 'ente.local' | Where-Object description -like "*nieobecność pow. 14 dni*" | select Company, SamAccountName, description)
-,(Get-ADUser -filter * -Properties SamAccountName, description, Company -Server 'fk.wasko.pl' | Where-Object description -like "*nieobecność pow. 14 dni*" | select Company, SamAccountName, description)
+ (Get-ADUser -filter * -Properties SamAccountName, description, Company -Server 'wasko' | Where-Object description -like "*nieob*" | select Company, SamAccountName, description)
+,(Get-ADUser -filter * -Properties SamAccountName, description, Company -Server 'ente' | Where-Object description -like "*nieob*" | select Company, SamAccountName, description)
+,(Get-ADUser -filter * -Properties SamAccountName, description, Company -Server 'fk' | Where-Object description -like "*nieob*" | select Company, SamAccountName, description)
     $back = Read-Host "Powrót do ekranu głównego ? T [Tak]"
     } while ($back -eq "T")
     break
@@ -126,7 +126,7 @@ switch ($case) {
 
     '6' {
      do {
-            $disabledUsers = Get-ADUser -Filter { Enabled -eq $false } -Credential $Credentials -Server $Server -SearchBase "ou=wasko,dc=wasko,dc=pl" | Select-Object Name
+            $disabledUsers = Get-ADUser -Filter { Enabled -eq $false } -Credential $Credentials -Server $Server -SearchBase "1" | Select-Object Name
 
             $disabledUsers | Format-List Name |  Out-File C:\list_users_disabled.txt
 
@@ -135,7 +135,7 @@ switch ($case) {
     $next5 = Read-Host "Czy przenieść konta do OU=wylaczone konta? (Tak/Nie)"
             
             if ($next5 -eq "Tak") {
-                Get-ADUser -Filter { Enabled -eq $false } -Credential $Credentials -Server $Server -SearchBase "dc=wasko,dc=pl" | Move-ADObject -TargetPath "ou=wylaczone konta,dc=wasko,dc=pl"
+                Get-ADUser -Filter { Enabled -eq $false } -Credential $Credentials -Server $Server -SearchBase "1" | Move-ADObject -TargetPath "1"
             }
 
             Write-Host -ForegroundColor DarkYellow "End file"
@@ -164,7 +164,7 @@ switch ($case) {
 '8' {
     do {
         # Pobierz użytkowników bez menedżera
-        $users = Get-ADUser -LDAPFilter "(!(manager=*))" -Properties Name -Credential $Credentials_Ente -Server $Server_Ente -SearchBase "OU=ente, DC=ente, DC=local"
+        $users = Get-ADUser -LDAPFilter "(!(manager=*))" -Properties Name -Credential $Credentials_Ente -Server $Server_Ente -SearchBase "1"
         
         # Wyświetl listę użytkowników
         if ($users) {
