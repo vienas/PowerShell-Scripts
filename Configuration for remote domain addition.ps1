c6208a53673d99faa7927779712ef3e8a4a09a4c
Vienas <67314023+vienas@users.noreply.github.com>
2025-03-19 11:26:54 +0100

    Update Configuration for remote domain addition.ps1

--- Configuration for remote domain addition.ps1
+++ Configuration for remote domain addition.ps1
@@ -3,7 +3,7 @@ Set-Service -Name dot3svc -Status Running -StartupType Automatic
 Ipconfig /flushdns
 Ipconfig /release
 Ipconfig /renew
-New-LocalUser -Name "Wasko" -Description "Wasko" -Password ( ConvertTo-SecureString "12Wasko3" -AsPlainText -Force)
+New-LocalUser -Name "Wasko" -Description "Wasko" -Password ( ConvertTo-SecureString "password" -AsPlainText -Force)
 Add-LocalGroupMember -Group "Administratorzy" -Member "Wasko"
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 0
 Set-NetFirewallProfile -Profile Domain, Public, Private -Enabled False
